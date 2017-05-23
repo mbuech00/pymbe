b670b2ad304677372dbb0ecc2d320a4aa7894329
Janus Juul Eriksen <januseriksen@gmail.com>
2017-05-23 09:33:15 +0200

    use HARD TABS in bg_main

--- bg_main.py
+++ bg_main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
-""" bg_main.py: python driver for Bethe-Goldstone correlation calculations.
+""" bg_main.py: python driver for Bethe-Goldstone correlation calculations. """
 
-written by Janus J. Eriksen (jeriksen@uni-mainz.de), 2016-2017, Mainz, Germany."""
+__author__ = 'Dr. Janus Juul Eriksen, JGU Mainz'
+__copyright__ = 'Copyright 2017'
+__credits__ = ['Prof. Juergen Gauss', 'Dr. Filippo Lipparini']
+__license__ = '???'
+__version__ = '0.8'
+__maintainer__ = 'Dr. Janus Juul Eriksen'
+__email__ = 'jeriksen@uni-mainz.de'
+__status__ = 'Development'
 
 from mpi4py import MPI
 
@@ -17,70 +24,35 @@ from bg_summary import summary_main
 from bg_driver import main_drv
 from bg_plotting import ic_plot
 
-__author__ = 'Dr. Janus Juul Eriksen, JGU Mainz'
-__copyright__ = 'Copyright 2017'
-__credits__ = ['Prof. Juergen Gauss', 'Dr. Filippo Lipparini']
-__license__ = '???'
-__version__ = '0.8'
-__maintainer__ = 'Dr. Janus Juul Eriksen'
-__email__ = 'jeriksen@uni-mainz.de'
-__status__ = 'Development'
-
 def main():
-   #
-   #  ---  init molecule dict...  ---
-   #
-   molecule = {}
-   #
-   #  ---  init mpi...  ---
-   #
-   init_mpi(molecule)
-   #
-   #  ---  master only...  ---
-   #
-   if (MPI.COMM_WORLD.Get_rank() == 0):
-      #
-      #  ---  initialize the calculation...  ---
-      #
-      init_calc(molecule)
-      #
-      #  ---  print program header...  ---
-      #
-      print_main_header(molecule,'bg_output.out',True)
-      #
-      #  ---  initialization done - start the calculation...  ---
-      #
-      main_drv(molecule)
-      #
-      #  ---  calculate timings...  ---
-      #
-      calc_mpi_timings(molecule)
-      #
-      #  ---  start (potential) reference calculation...  ---
-      #
-      if (molecule['ref'] and (not molecule['error'][-1])): ref_calc(molecule)
-      #
-      #  ---  print summary of the calculation  ---
-      #
-      summary_main(molecule)
-      #
-      #  ---  plot the results of the calculation  ---
-      #
-      if (not molecule['error'][-1]): ic_plot(molecule)
-      #
-      #  ---  terminate calculation and clean up...  ---
-      #
-      term_calc(molecule,True)
-      #
-      #  ---  print program end...  ---
-      #
-      print_main_end(molecule)
-   #
-   #  ---  finalize mpi...  ---
-   #
-   finalize_mpi(molecule)
+		""" main bg program """
+		# init molecule dict
+		molecule = {}
+		# init mpi
+		init_mpi(molecule)
+		# master only
+		if (MPI.COMM_WORLD.Get_rank() == 0):
+			# initialize the calculation
+			init_calc(molecule)
+			# print program header
+			print_main_header(molecule,'bg_output.out',True)
+			# initialization done - start the calculation
+			main_drv(molecule)
+			# calculate timings
+			calc_mpi_timings(molecule)
+			# do (potential) reference calculation
+			if (molecule['ref'] and (not molecule['error'][-1])): ref_calc(molecule)
+			# print summary of the calculation
+			summary_main(molecule)
+			# plot results
+			if (not molecule['error'][-1]): ic_plot(molecule)
+			# terminate calculation and clean up
+			term_calc(molecule,True)
+			# print program end
+			print_main_end(molecule)
+		# finalize mpi
+		finalize_mpi(molecule)
 
 if __name__ == '__main__':
-   #
-   main()
+	main()
 
