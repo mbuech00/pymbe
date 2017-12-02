0124535930cccd5de48559a637011cd4a08f2898
Janus Juul Eriksen <januseriksen@gmail.com>
2017-12-02 23:19:40 +0100

    e-05 accurqcy

--- bg_kernel.py
+++ bg_kernel.py
@@ -82,7 +82,7 @@ class KernCls():
 				_exp.energy_tot.append(e_tmp)
 				# check for convergence wrt total energy
 				if ((_exp.order > _exp.start_order) and \
-					(abs(_exp.energy_tot[-1] - _exp.energy_tot[-2]) < 1.0e-06)):
+					(abs(_exp.energy_tot[-1] - _exp.energy_tot[-2]) < 1.0e-05)):
 					_exp.conv_energy.append(True)
 				#
 				return
