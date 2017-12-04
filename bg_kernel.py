ca37c0d5d3704db44b1449a920dc53428dd887c5
Janus Juul Eriksen <januseriksen@gmail.com>
2017-12-04 12:55:46 +0100

    only one energy conv check

--- bg_kernel.py
+++ bg_kernel.py
@@ -82,8 +82,7 @@ class KernCls():
 				_exp.energy_tot.append(e_tmp)
 				# check for convergence wrt total energy
 				if ((_exp.order > _exp.start_order+1) and \
-					(abs(_exp.energy_tot[-1] - _exp.energy_tot[-2]) < _exp.thres) and \
-					(abs(_exp.energy_tot[-2] - _exp.energy_tot[-3]) < _exp.thres)):
+					(abs(_exp.energy_tot[-1] - _exp.energy_tot[-2]) < _exp.thres)):
 						_exp.conv_energy.append(True)
 				#
 				return
