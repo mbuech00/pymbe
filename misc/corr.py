2dc100a3e0a924cba5f7a55c9eec307d41deaefc
Janus Juul Eriksen <januseriksen@gmail.com>
2018-03-26 16:32:26 +0200

    changed corr.py

--- misc/corr.py
+++ misc/corr.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import numpy as np
+import subprocess
 
 if len(sys.argv) != 2:
     sys.exit('\n error: output directory must be specified: python plot.py out_dir\n')
@@ -13,21 +14,13 @@ out_dir = sys.argv[1]
 if not os.path.isdir(out_dir):
     sys.exit('\n error: out_dir argument is not a directory\n')
 
-e_hf = e_fci = None
+e_corr = None
 
 with open(out_dir+'/results.out') as f:
-    for line in f:
-        if 'Hartree-Fock energy' in line:
-            e_hf = float(line.split()[-1])
-        elif 'final MBE energy' in line:
-            e_fci = float(line.split()[-1])
-        else:
-            continue
+	e_corr = float(subprocess.check_output(['tail', '-4', out_dir+'/results.out']).split()[2])
 
-if e_hf is None or e_fci is None:
-    sys.exit('\n error: hartree-fock and/or mbe-fci energies were not found on file\n')
-
-e_corr = e_fci - e_hf
+if e_corr is None:
+    sys.exit('\n error: correlation energies were not found on file\n')
 
 print('{0:} has e_corr = {1:.5f}'.format(out_dir, e_corr))
 
