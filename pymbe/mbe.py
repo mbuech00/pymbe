57a3d681f17dd12ed65c6127ee655407e6db690b
Janus Juul Eriksen <januseriksen@gmail.com>
2018-03-29 20:43:02 +0200

    new load balancing scheme in mbe

--- pymbe/mbe.py
+++ pymbe/mbe.py
@@ -111,14 +111,16 @@ def _master(mpi, mol, calc, exp):
 		job_info = {}
 		# init job index
 		i = np.argmax(np.isnan(exp.energy['inc'][-1]))
-		# init stat counter
-		counter = i
+		# init stat and restart counters
+		counter_stat = counter_rst = i
 		# print status for START
-		if mpi.global_master: output.mbe_status(exp, float(counter) / float(len(exp.tuples[-1])))
+		if mpi.global_master: output.mbe_status(exp, float(i) / float(len(exp.tuples[-1])))
 		# init time
 		if mpi.global_master:
 			if len(exp.time['mbe']) < (exp.order - exp.start_order) + 1: exp.time['mbe'].append(0.0)
 			time = MPI.Wtime()
+		# init tasks
+		tasks = _tasks(len(exp.tuples[-1]), num_slaves)
 		# loop until no slaves left
 		while (slaves_avail >= 1):
 			# receive data dict
@@ -133,35 +135,39 @@ def _master(mpi, mol, calc, exp):
 			if tag == TAGS.ready:
 				# any jobs left?
 				if i <= len(exp.tuples[-1]) - 1:
-					# store job index
-					job_info['index'] = i
+					if tasks[source-1]:
+						batch = tasks[source-1].pop(0)
+					else:
+						batch = 1
+					# store job indices
+					job_info['i_s'] = i; job_info['i_e'] = i + batch
 					# send string dict
 					comm.send(job_info, dest=source, tag=TAGS.start)
 					# increment job index
-					i += 1
+					i += batch
 				else:
 					# send exit signal
 					comm.send(None, dest=source, tag=TAGS.exit)
 			# receive result from slave
 			elif tag == TAGS.done:
 				# collect energies
-				exp.energy['inc'][-1][data['index']] = data['e_inc']
-				# write restart files
+				for idx, val in enumerate(data['e_inc']):
+					exp.energy['inc'][-1][data['i_s']+idx] = val
 				if mpi.global_master:
-					if (data['index'] + 1) % exp.rst_freq == 0 or mol.verbose:
+					# write restart files
+					if data['i_e'] // exp.rst_freq > counter_rst:
+						counter_rst += 1
 						restart.mbe_write(calc, exp, False)
-				# increment stat counter
-				counter += 1
-				# print status
-				if mpi.global_master:
-					if (data['index'] + 1) % 1000 == 0 or mol.verbose:
-						output.mbe_status(exp, float(counter) / float(len(exp.tuples[-1])))
+					# print status
+					if data['i_e'] // 1000 > counter_stat or mol.verbose:
+						counter_stat += 1
+						output.mbe_status(exp, float(i) / float(len(exp.tuples[-1])))
 			# put slave to sleep
 			elif tag == TAGS.exit:
 				slaves_avail -= 1
 		# print 100.0 %
 		if mpi.global_master and not mol.verbose:
-			if len(exp.tuples[-1]) % 10 != 0:
+			if len(exp.tuples[-1]) % 1000 != 0:
 				output.mbe_status(exp, 1.0)
 		# bcast energies
 		parallel.energy(exp, comm)
@@ -188,26 +194,31 @@ def _slave(mpi, mol, calc, exp):
 			tag = mpi.stat.Get_tag()
 			# do job
 			if tag == TAGS.start:
-				# generate input
-				exp.core_idx, exp.cas_idx = kernel.core_cas(mol, exp, exp.tuples[-1][job_info['index']])
-				# perform calc
-				e_model = kernel.corr(mol, calc, exp, calc.model['METHOD']) \
-							+ (calc.energy['hf'] - calc.energy['ref'])
-				if calc.base['METHOD'] is None:
-					e_base = 0.0
-				else:
-					e_base = kernel.corr(mol, calc, exp, calc.base['METHOD']) \
-								+ (calc.energy['hf'] - calc.energy['ref_base'])
-				exp.energy['inc'][-1][job_info['index']] = e_model - e_base
-				# calc increment
-				exp.energy['inc'][-1][job_info['index']] -= _sum(calc, exp, job_info['index'])
-				# verbose print
-				if mol.verbose:
-					print(' core = {0:} , cas = {1:} , e_model = {2:.4e} , e_base = {3:.4e} , e_inc = {4:.4e}'.\
-							format(exp.core_idx, exp.cas_idx, e_model, e_base, exp.energy['inc'][-1][job_info['index']]))
-				# write info into data dict
-				data['index'] = job_info['index']
-				data['e_inc'] = exp.energy['inc'][-1][job_info['index']]
+				# init data['e_inc']
+				data['e_inc'] = np.zeros(job_info['i_e']-job_info['i_s'], dtype=np.float64)
+				# calculate energy increments
+				for count, idx in enumerate(range(job_info['i_s'], job_info['i_e'])):
+					# generate input
+					exp.core_idx, exp.cas_idx = kernel.core_cas(mol, exp, exp.tuples[-1][idx])
+					# perform calc
+					e_model = kernel.corr(mol, calc, exp, calc.model['METHOD']) \
+								+ (calc.energy['hf'] - calc.energy['ref'])
+					if calc.base['METHOD'] is None:
+						e_base = 0.0
+					else:
+						e_base = kernel.corr(mol, calc, exp, calc.base['METHOD']) \
+									+ (calc.energy['hf'] - calc.energy['ref_base'])
+					exp.energy['inc'][-1][idx] = e_model - e_base
+					# calc increment
+					exp.energy['inc'][-1][idx] -= _sum(calc, exp, idx)
+					# verbose print
+					if mol.verbose:
+						print(' core = {0:} , cas = {1:} , e_model = {2:.4e} , e_base = {3:.4e} , e_inc = {4:.4e}'.\
+								format(exp.core_idx, exp.cas_idx, e_model, e_base, exp.energy['inc'][-1][idx]))
+					# write info into data dict
+					data['e_inc'][count] = exp.energy['inc'][-1][idx]
+				# store indices
+				data['i_s'] = job_info['i_s']; data['i_e'] = job_info['i_e']
 				# send data back to local master
 				comm.send(data, dest=0, tag=TAGS.done)
 			# exit
@@ -241,5 +252,14 @@ def _comb_index(n, k):
 		count = scipy.misc.comb(n, k, exact=True)
 		index = np.fromiter(itertools.chain.from_iterable(itertools.combinations(range(n), k)), int,count=count * k)
 		return index.reshape(-1, k)
+
+
+def _tasks(size, slaves):
+		""" determine batch sizes """
+		b1 = max(1, (size//10*9) // slaves) #  0 % - 90 %
+		b2 = max(1, ((size//20*19) - (size//10*9)) // slaves // 2) #  90 % - 95 %
+		b4 = max(1, ((size//50*49) - (size//20*19)) // slaves // 6) #  95 % - 98 %
+		b6 = max(1, ((size//100*99) - (size//50*49)) // slaves // 6) #  98 % - 100 %
+		return [[b1] + [b2]*2 + [b4]*4 + [b6]*6 for idx in range(slaves)]
 
 
