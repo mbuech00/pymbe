75b4602b59f83e2af68fa85fd1131f2164fddf46
Janus Juul Eriksen <januseriksen@gmail.com>
2019-05-21 16:03:37 +0100

    pep-8

--- misc/make_system.py
+++ misc/make_system.py
@@ -15,24 +15,24 @@ b = float(sys.argv[4])
 
 
 def main(shape, atom_type, n_atoms, b):
-		""" monoatomic system of given shape with bond lengths b """
-		string = '\n'
-		form = ()
-		if shape == 'chain':
-			# chain
-			for z in np.linspace(-n_atoms * b / 2. + b / 2., n_atoms * b / 2. - b / 2., num=n_atoms):
-				string += '{:<3s} {:>10.5f} {:>10.5f} {:>10.5f}\n'
-				form += (atom_type.upper(), 0.0, 0.0, z,)
-		elif shape == 'ring':
-			# ring (adapted from pyscf/tools/ring.py)
-			r = b / 2. / np.sin(np.pi / n_atoms)
-			for atom in range(n_atoms):
-				theta = atom * (2. * np.pi / n_atoms)
-				string += '{:<3s} {:>10.5f} {:>10.5f} {:>10.5f}\n'
-				form += (atom_type.upper(), r * np.cos(theta), r * np.sin(theta), 0.0,)
-		else:
-			sys.exit('\n unknown shape. currently implemented: chain and ring')
-		return string.format(*form)
+        """ monoatomic system of given shape with bond lengths b """
+        string = '\n'
+        form = ()
+        if shape == 'chain':
+            # chain
+            for z in np.linspace(-n_atoms * b / 2. + b / 2., n_atoms * b / 2. - b / 2., num=n_atoms):
+                string += '{:<3s} {:>10.5f} {:>10.5f} {:>10.5f}\n'
+                form += (atom_type.upper(), 0.0, 0.0, z,)
+        elif shape == 'ring':
+            # ring (adapted from pyscf/tools/ring.py)
+            r = b / 2. / np.sin(np.pi / n_atoms)
+            for atom in range(n_atoms):
+                theta = atom * (2. * np.pi / n_atoms)
+                string += '{:<3s} {:>10.5f} {:>10.5f} {:>10.5f}\n'
+                form += (atom_type.upper(), r * np.cos(theta), r * np.sin(theta), 0.0,)
+        else:
+            sys.exit('\n unknown shape. currently implemented: chain and ring')
+        return string.format(*form)
 
 
 print(main(shape, atom_type, n_atoms, b))
