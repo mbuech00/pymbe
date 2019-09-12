ae4d34ead9b479a4c88a31a5bf92cdeeb079c4ff
Janus Juul Eriksen <januseriksen@gmail.com>
2019-09-12 20:24:30 +0100

    more debugging

--- pymbe/mbe.py
+++ pymbe/mbe.py
@@ -78,6 +78,7 @@ def master(mpi, mol, calc, exp):
                 inc = np.ndarray(buffer=buf, dtype=np.float64, shape=(exp.n_tasks[-1],))
             elif calc.target in ['dipole', 'trans']:
                 inc = np.ndarray(buffer=buf, dtype=np.float64, shape=(exp.n_tasks[-1], 3))
+                inc[:] = np.zeros_like(inc, dtype=np.float64)
 
             # save increments
             if calc.misc['rst']:
@@ -250,6 +251,7 @@ def slave(mpi, mol, calc, exp):
             inc.append(np.ndarray(buffer=buf, dtype=np.float64, shape=(exp.n_tasks[-1],)))
         elif calc.target in ['dipole', 'trans']:
             inc.append(np.ndarray(buffer=buf, dtype=np.float64, shape=(exp.n_tasks[-1], 3)))
+        inc[-1][:] = np.zeros_like(inc[-1], dtype=np.float64)
 
         # load hashes for current and previous orders
         hashes = []
