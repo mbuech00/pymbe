f28321b56ae75c79d7d267d7395dfb977a41cf92
Janus Juul Eriksen <januseriksen@gmail.com>
2019-10-31 22:57:13 +0000

    change to coov pruning

--- src/tools.py
+++ src/tools.py
@@ -30,7 +30,7 @@ import parallel
 RST = os.getcwd()+'/rst'
 # pi-orbitals
 PI_SYMM_D2H = np.array([2, 3, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21, 22, 23, 24, 25, 26, 27])
-PI_SYMM_C2V = np.array([2, 3, 10, 11, 12, 13, 20, 21, 22, 23])
+PI_SYMM_C2V = np.array([10, 11, 12, 13, 20, 21, 22, 23])
 
 
 class Logger:
