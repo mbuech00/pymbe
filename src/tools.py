97a44cc9b017740d98f8b954f3ba43947ccf30e3
Janus Juul Eriksen <januseriksen@gmail.com>
2020-01-29 08:56:32 +0000

    change PI_SYMM_C2V

--- src/tools.py
+++ src/tools.py
@@ -29,7 +29,7 @@ from typing import Tuple, Set, List, Dict, Any, Generator, Union
 RST = os.getcwd()+'/rst'
 # pi-orbitals
 PI_SYMM_D2H = np.array([2, 3, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21, 22, 23, 24, 25, 26, 27])
-PI_SYMM_C2V = np.array([10, 11, 12, 13, 20, 21, 22, 23])
+PI_SYMM_C2V = np.array([2, 3, 10, 11, 12, 13, 20, 21, 22, 23])
 
 
 class Logger:
