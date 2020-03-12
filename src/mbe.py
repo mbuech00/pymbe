782cbcf4dbdd0c95eb1ff93a8d8eccf086eac3b9
Janus Juul Eriksen <januseriksen@gmail.com>
2020-03-12 08:47:26 +0000

    restart fix

--- src/mbe.py
+++ src/mbe.py
@@ -125,11 +125,13 @@ def main(mpi: MPICls, mol: MolCls, calc: CalcCls, exp: ExpCls, \
         rst_write = calc.misc['rst'] and mpi.global_size < calc.misc['rst_freq'] < exp.n_tuples['prop'][-1]
 
         # start tuples
-        if tup_a is None:
-            tup_occ_a = tup_virt_a = None
-        else:
+        if tup_a is not None:
             tup_occ_a = tup_a[tup_a < mol.nocc]
             tup_virt_a = tup_a[mol.nocc <= tup_a]
+        if tup_occ_a.size == 0:
+            tup_occ_a = None
+        if tup_virt_a.size == 0:
+            tup_virt_a = None
 
         # loop until no tuples left
         if tup_idx_a < exp.n_tuples['prop'][-1]:
